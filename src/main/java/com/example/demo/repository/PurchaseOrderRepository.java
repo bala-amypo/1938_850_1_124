3884993b2401a84870e812c97baca0fbd6f4e7ca
bala-amypo <bala@amypo.in>
2025-12-22 07:41:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/PurchaseOrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+    List<PurchaseOrder> findBySupplier_Id(Long supplierId);
+    List<PurchaseOrder> findByCategory_Id(Long categoryId);
+}
