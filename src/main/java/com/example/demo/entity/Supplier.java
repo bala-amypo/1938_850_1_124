75010733df87f1dcafa12566ddf187535dfb6efa
bala-amypo <bala@amypo.in>
2025-12-23 04:44:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/Supplier.java
+++ src/main/java/com/example/demo/entity/Supplier.java
@@ -55,13 +55,6 @@ public class Supplier {
     }
 
     // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -18,8 +18,7 @@ public class UserAccount {
     
     public UserAccount() {}
     
-    public UserAccount(Long id, String email, String password, String role, String fullName) {
-        this.id = id;
+    public UserAccount(, String email, String password, String role, String fullName) {
         this.email = email;
         this.password = password;
         this.role = role;
@@ -27,9 +26,6 @@ public class UserAccount {
     }
     
     // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     
