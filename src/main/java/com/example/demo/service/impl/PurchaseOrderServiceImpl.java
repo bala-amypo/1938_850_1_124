f92b4db4716c6d71dceb216fe4a2a524e345ab40
bala-amypo <bala@amypo.in>
2025-12-26 08:23:27 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PurchaseOrderServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PurchaseOrderServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.impl;
+package com.example.demo.service.impl.PurchaseOrderServiceImpl;
 
 import com.example.demo.entity.PurchaseOrder;
 import com.example.demo.entity.Supplier;
