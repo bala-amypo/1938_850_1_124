ef695d3a4b3c17416e1c1acff37b306be3ad6d03
bala-amypo <bala@amypo.in>
2025-12-26 13:14:59 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountService.java
+++ src/main/java/com/example/demo/service/UserAccountService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.UserAccount;
-@Service
+
+// REMOVE the @Service annotation from here
 public interface UserAccountService {
     UserAccount register(UserAccount user);
     String login(String email, String password);
