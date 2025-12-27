490bf214871a19bd06052132e09eefd2418c2690
bala-amypo <bala@amypo.in>
2025-12-27 06:11:28 +0000

    User commit

--- src/main/java/com/example/demo/service/SpendCategoryService.java
+++ src/main/java/com/example/demo/service/SpendCategoryService.java
@@ -5,19 +5,6 @@ import java.util.List;
 
 public interface SpendCategoryService {
     SpendCategory createCategory(SpendCategory category);
-    // Add these two:
-    List<SpendCategory> getAllCategories(); 
-    void deactivateCategory(Long id);
-}
-
-
-package com.example.demo.service;
-
-import com.example.demo.entity.SpendCategory;
-import java.util.List;
-
-public interface SpendCategoryService {
-    SpendCategory createCategory(SpendCategory category);
     List<SpendCategory> getAllCategories();
     void deactivateCategory(Long id);
     
